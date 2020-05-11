b4c5075d0c0249cffa99c83ece624590361f0701
michaelleriche <mikeleriche@gmail.com>
2020-05-10 22:34:43 -0400

    add page refresh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 const cellsHorizontal = 15;
 const cellsVertical = 10;
 const width = window.innerWidth;
-const height = window.innerHeight;
+const height = window.innerHeight - 100;
 
 const unitLengthX = width / cellsHorizontal;
 const unitLengthY = height / cellsVertical;
@@ -188,16 +188,16 @@ World.add(world, ball);
 
 document.addEventListener('keydown', event => {
     const { x, y} = ball.velocity;
-    if (event.keyCode === 87) {
+    if (event.keyCode === 38) {
         Body.setVelocity(ball, { x, y: y - 5 })
     }
-    if (event.keyCode === 68) {
+    if (event.keyCode === 39) {
         Body.setVelocity(ball, { x: x + 5, y })
     }
-    if (event.keyCode === 83) {
+    if (event.keyCode === 40) {
         Body.setVelocity(ball, { x, y: y + 5 })
     }
-    if (event.keyCode === 65) {
+    if (event.keyCode === 37) {
         Body.setVelocity(ball, { x: x - 5, y })
     }
 });
@@ -222,3 +222,7 @@ Events.on(engine, 'collisionStart', event => {
         }
     })
 });
+
+function refreshPage () {
+    location.reload();
+}
